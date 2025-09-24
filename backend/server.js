c6d8c53f78c923aa469d9a5e245d6b3997611994
Kauapy <kauadecarvalho20@gmail.com>
2025-09-24 20:22:08 -0300

    Adicionar rota para livros e configurar o servidor

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const express = require("express")
 const app = express()
+const rotaLivro = require("./rotas/livros")
 const port = 5000
 
-app.get(("/"), (req, res) => {
-    
-})
+app.use("/livros", rotaLivro)
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`);
